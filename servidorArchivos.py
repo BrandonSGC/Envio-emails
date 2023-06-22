9d0bca6e5331b9011bcb4020c7782a1efdd9b4b6
BrandonSGC <brangmz20@gmail.com>
2023-06-21 19:54:41 -0600

    Lee los 3 archivos XML

--- servidorArchivos.py
+++ servidorArchivos.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 def main():
     host = 'localhost'
@@ -10,33 +11,36 @@ def main():
         servidor.listen(1)
         print('Servidor en espera de conexiones...')
 
-        conexion, direccion = servidor.accept()
-        print('Cliente conectado:', direccion)
-
         while True:
-            nombre_archivo = conexion.recv(1024).decode()
-            if not nombre_archivo:
-                break
+            conexion, direccion = servidor.accept()
+            print('Cliente conectado:', direccion)
 
-            tamano_archivo = conexion.recv(4)  # Recibe el tamaño del archivo en 4 bytes
-            tamano_archivo = int.from_bytes(tamano_archivo, byteorder='big')  # Convierte los bytes a un entero
+            while True:
+                # Recibe el nombre del archivo
+                nombre_archivo = conexion.recv(1024).decode()
+                if not nombre_archivo:
+                    break
 
-            contenido_archivo = b''
-            bytes_recibidos = 0
-            while bytes_recibidos < tamano_archivo:
-                datos = conexion.recv(min(tamano_archivo - bytes_recibidos, 1024))
-                contenido_archivo += datos
-                bytes_recibidos += len(datos)
+                # Recibe la longitud del archivo en 4 bytes
+                longitud_archivo = conexion.recv(4)
+                longitud_archivo = struct.unpack('!I', longitud_archivo)[0]
 
-            # Imprime el nombre y contenido del archivo XML
-            print(f"Archivo recibido: {nombre_archivo}")
-            print("Contenido:")
-            print(contenido_archivo.decode())
+                # Recibe el contenido del archivo
+                contenido_archivo = conexion.recv(longitud_archivo).decode()
 
-        conexion.close()
-        servidor.close()
+                # Imprime el nombre y contenido del archivo XML
+                print(f"Archivo recibido: {nombre_archivo}")
+                print("Contenido:")
+                print(contenido_archivo)
+
+            conexion.close()
+            print('Cliente desconectado:', direccion)
+
     except socket.error as e:
         print("Error al iniciar el servidor:", str(e))
+
+    finally:
+        servidor.close()
 
 if __name__ == '__main__':
     main()
