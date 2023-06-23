f07971dfa1db7a16036e19a691e2f906909fa1e3
BrandonSGC <brangmz20@gmail.com>
2023-06-22 19:31:24 -0600

    Funcionalidad generar xml de estadisticas.

--- Servidor.py
+++ Servidor.py
@@ -10,6 +10,11 @@ host = 'localhost'
 port = 12345
 buffer_size = 1024
 
+# Variables de estadisticas.
+estadisticas = []
+totalEnviar = 0
+conProblemas = 0
+
 # Función para recibir los datos del email / archivos.
 def recibirDatos(client_socket):
     asunto = client_socket.recv(buffer_size).decode("utf-8")
@@ -20,11 +25,52 @@ def recibirDatos(client_socket):
     client_socket.close()
     return asunto, mensaje, archivo1, archivo2, archivo3
 
+def generarXML(estadisticas_archivo):
+    # Crear el documento XML
+    doc = minidom.Document()
+
+    # Crear la etiqueta raíz
+    root = doc.createElement("root")
+    doc.appendChild(root)
+
+    for estadistica_archivo in estadisticas_archivo:
+        # Crear el elemento 'estadisticas'
+        estadisticas_elem = doc.createElement("estadisticas")
+        root.appendChild(estadisticas_elem)
+
+        archivo = doc.createElement('archivo')
+        estadisticas_elem.appendChild(archivo)
+        archivoTexto = doc.createTextNode(estadistica_archivo['archivo'])
+        archivo.appendChild(archivoTexto)
+
+        totalenvios = doc.createElement('totalenvios')
+        estadisticas_elem.appendChild(totalenvios)
+        totalenviosTexto = doc.createTextNode(str(estadistica_archivo['totalenvios']))
+        totalenvios.appendChild(totalenviosTexto)
+
+        fallidos = doc.createElement('fallidos')
+        estadisticas_elem.appendChild(fallidos)
+        fallidosTexto = doc.createTextNode(str(estadistica_archivo['fallidos']))
+        fallidos.appendChild(fallidosTexto)
+
+    # Guardar el documento XML en un archivo
+    xml_file = open("Estadisticas.xml", "w")
+    xml_file.write(doc.toprettyxml(indent="\t"))
+    xml_file.close()
+    print('El archivo se ha creado con éxito!')
+
+
+
 
 # Funcion que envia los correos a cada persona en el archivo.
-def enviarCorreos(archivo, remitente, asunto, mensaje):
-    print(f'Enviando correos desde el archivo: {archivo}')
-    
+def enviarCorreos(archivo, remitente, asunto, mensaje):      
+    # Variables para almacenar las estadísticas del archivo actual
+    estadisticas_archivo = {
+        'archivo': archivo.split('\\')[-1],  # Obtener el nombre del archivo
+        'totalenvios': 0,
+        'fallidos': 0
+    }
+
     if os.path.exists(archivo):
         doc = minidom.parse(archivo)
 
@@ -36,31 +82,45 @@ def enviarCorreos(archivo, remitente, asunto, mensaje):
 
         # Iterar sobre los elementos "personne"
         for persona in personas:
+            
             # Obtener los valores de cada etiqueta
             firstname = persona.getElementsByTagName("firstname")[0].childNodes[0].data
             lastname = persona.getElementsByTagName("lastname")[0].childNodes[0].data
             phone = persona.getElementsByTagName("phone")[0].childNodes[0].data
             destinatario = persona.getElementsByTagName("email")[0].childNodes[0].data
 
-            email = EmailMessage()
-            email["From"] = remitente
-            email["To"] = destinatario
-            email["Subject"] = asunto
+            try:
+                email = EmailMessage()
+                email["From"] = remitente
+                email["To"] = destinatario
+                email["Subject"] = asunto
 
-            email.set_content(mensaje)
+                email.set_content(mensaje)
 
-            smtp = smtplib.SMTP_SSL("smtp.gmail.com")
-            # Adjunatmos el correo del remitente y la clave.
-            smtp.login(remitente, "fiqosavbtvqwdcay")
-            smtp.sendmail(remitente, destinatario, email.as_string())
-            smtp.quit()
-            print(f"El correo electronico a {destinatario} de {firstname} {lastname} ha sido enviado con éxito!")
-            print("-"*70)
+                smtp = smtplib.SMTP_SSL("smtp.gmail.com")
+                # Adjunatmos el correo del remitente y la clave.
+                smtp.login(remitente, "fiqosavbtvqwdcay")
+                smtp.sendmail(remitente, destinatario, email.as_string())
+                # Incrementar el contador de envíos exitosos
+                estadisticas_archivo['totalenvios'] += 1
+                
+                smtp.quit()
+                print(f"El correo electronico a {destinatario} de {firstname} {lastname} ha sido enviado con éxito!")
+                print("-"*70)
+            except:
+                # Incrementar el contador de envíos exitosos
+                estadisticas_archivo['fallidos'] += 1                
+                print(f"Ha ocurrido un error al enviar el correo electrónico a {destinatario}")
+                print("-" * 70)
+                        
+        # Agregar las estadísticas del archivo actual a la lista de estadísticas
+        estadisticas.append(estadisticas_archivo)
+        
     else:
         print(f'No se ha encontrado el archivo: {archivo}')
 
 
-def start_server():
+def start_server():    
     # Crear un socket TCP/IP
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -100,7 +160,18 @@ def start_server():
         hilo2.start()
         hilo3.start()
 
+        # Esperar a que los hilos terminen antes de continuar
+        hilo1.join()
+        hilo2.join()
+        hilo3.join()
+        
+        break
+    
+    print('Se mandaron todos los emails.')
+    # Generar el archivo XML con las estadísticas
+    generarXML(estadisticas)
 
 os.system('cls')
+
 # Iniciar el servidor
 start_server()
